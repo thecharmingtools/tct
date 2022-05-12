2d0ff91e5279f600889c345ebfa06210707b623a
MorgeMoensch <morgemoensch@morgemoensch.dev>
2022-05-12 19:40:38 +0200

    Delay background-colour of navbar and add smoth fade-in

--- v2/js/scripts.js
+++ v2/js/scripts.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', event => {
         if (!navbarCollapsible) {
             return;
         }
-        if (window.scrollY === 0) {
+        if (window.scrollY <= 200) {
             navbarCollapsible.classList.remove('navbar-shrink')
         } else {
             navbarCollapsible.classList.add('navbar-shrink')
