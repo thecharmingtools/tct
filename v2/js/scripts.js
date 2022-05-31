1831b61f5ec0d089b121476505b87c2bdac0f755
MorgeMoensch <morgemoensch@morgemoensch.dev>
2022-05-31 19:47:48 +0200

    Remove unused imports. Fix Mobile Menu

--- v2/js/scripts.js
+++ v2/js/scripts.js
@@ -29,15 +29,6 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
     const responsiveNavItems = [].slice.call(
@@ -50,10 +41,14 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
 });
+
+function changeMobileMenuVisibility() {
+    let menu = document.getElementById("navbarResponsive");
+
+    if(menu.classList.contains("show")) {
+        menu.classList.remove("show")
+    } else {
+        menu.classList.add("show")
+    }
+}
